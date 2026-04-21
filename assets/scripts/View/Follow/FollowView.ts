198d3c2f2cbc644fb7d0124cb65414b02f849aa0
cyybhzw <50670854+cyybhzw@users.noreply.github.com>
2026-04-21 15:24:24 +0800

    朋友圈

--- assets/scripts/View/Follow/FollowView.ts
+++ assets/scripts/View/Follow/FollowView.ts
@@ -100,7 +100,7 @@ export class FollowView extends Component {
                 let img = imageURL[i]
                 const page = instantiate(this.pagePrefab);
                 content.addChild(page);
-
+                this.pageView.addPage(page)
                 const imgSp = page.getChildByName("banner").getComponent(Sprite);
                 if (imgSp) {
                     let journalImg = AppConst.JournalManager.journalImgs.find((i) => i.type == "localImg" && i.id == img["id"])
